c40a7443c10fb72fc2e5f478af211a3bbeae5902
Matias <matiasaramayo@hotmail.com>
2022-06-10 14:24:24 -0300

    Carrousel aplicando los contenidos de React

--- src/Components/Carrousel/Carrousel.jsx
+++ src/Components/Carrousel/Carrousel.jsx
@@ -1,58 +1,32 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
-import img1 from '../../img/img1.jpg'
-import img2 from '../../img/img2.jpg'
-import img3 from '../../img/img3.jpg'
-import img4 from '../../img/img4.jpg'
+import "@splidejs/react-splide/css";
+import "@splidejs/react-splide/css/core";
+import "./Carrousel.css";
 
-import '@splidejs/react-splide/css';
-import '@splidejs/react-splide/css/core';
-import './Carrousel.css'
-
-
-
+import { arrImg } from "../../../mock/carrouselMock";
+import CarrouselInfo from "./CarrouselInfo/CarrouselInfo";
 
 const Carrousel = () => {
-  const arrImg = [
-      {
-        id: 1,
-        img: [img1], 
-        text: "Hola"
-      },
-      {
-        id : 2,
-        img: [img2],
-        text: "Chau"
-      },
-      {
-        id:3 ,
-        img : [img3],
-        text: "tito"
-      },
-      {
-        id:4,
-        img : [img4],
-        text: "el pity"
-      }
-  ]
+  const [carImg, setCarImg] = useState([]);
+
+  useEffect(() => {
+    setCarImg(arrImg);
+  }, [arrImg]);
+
   return (
     <div>
-      <Splide className="splide" options={{ rewind: true , arrows: true, pagination:true}} aria-label="React Splide Example">
-        
-        {
-          arrImg.map(arr => (
-
-            <SplideSlide className="splide-flex" key={arr.id}>
-              <img src={arr.img}  alt="Image 1" className="img-splide" />
-              <div className="text-flex">
-              <h1>{arr.text}</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ratione laborum voluptatem voluptate veritatis tenetur vero, sed, perferendis numquam suscipit sapiente accusamus ipsam magni aliquam odio recusandae quas in eos.</p>
-              </div>
-            </SplideSlide>
-          ))
-        }
-  
+      <Splide
+        className="splide"
+        options={{ rewind: true, arrows: true, pagination: true }}
+        aria-label="React Splide Example"
+      >
+        {carImg.map((arr) => (
+          <SplideSlide className="splide-flex" key={arr.id}>
+            <CarrouselInfo arr={arr} />
+          </SplideSlide>
+        ))}
       </Splide>
     </div>
   );
