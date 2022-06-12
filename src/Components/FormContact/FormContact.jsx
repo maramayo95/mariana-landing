2edbebdc7034b138807465fe45f5ef12c19de2bf
Matias <matiasaramayo@hotmail.com>
2022-06-12 18:50:15 -0300

    armando el diseño desktop form

--- src/Components/FormContact/FormContact.jsx
+++ src/Components/FormContact/FormContact.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 import "./FormContact.css";
+import arrowBtn from "../../svg/arrow.svg";
 const FormContact = () => {
   return (
     <div className="center-container">
-
       <section className="form-design">
         <form action="">
           <div className="title-form">
             <h3>Contacto</h3>
           </div>
-          <div className="input-div">
-            <input type="text" placeholder="Escribí acá tu nombre y apellido" />
-            <input type="text" placeholder="Escribí acá tú dirección de mail" />
-            <input type="text" placeholder="Escribí acá el asunto de tu consulta" />
-          </div>
+          <div className="input-group">
+            <div className="input-div">
+              <input
+                type="text"
+                placeholder="Escribí acá tu nombre y apellido"
+              />
+              <input
+                type="text"
+                placeholder="Escribí acá tú dirección de mail"
+              />
+              <input
+                type="text"
+                placeholder="Escribí acá el asunto de tu consulta"
+              />
+            </div>
+            <div className="align-text-form">
+              <div className="textA-div">
+                <textarea type="text" placeholder="Escribí acá tú mensaje" />
+              </div>
 
-          <div className="textA-div">
-            <textarea type="text" placeholder="Escribí acá tú mensaje" />
-          </div>  
-
-          <div className="div-btn-form">
-            <button className="btn-form" type="submit">Enviar Consulta</button>
+              <div className="div-btn-form">
+                <button className="btn-form" type="submit">
+                  <img src={arrowBtn} /> Enviar Consulta
+                </button>
+              </div>
+            </div>
           </div>
         </form>
       </section>
