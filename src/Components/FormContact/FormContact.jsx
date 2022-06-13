f6fb1bf89a17aeb2c9649db9c8f2ff361256723f
Matias Aramayo <matiasaramayo@hotmail.com>
2022-06-12 22:55:38 -0300

    Form terminado

--- src/Components/FormContact/FormContact.jsx
+++ src/Components/FormContact/FormContact.jsx
@@ -1,32 +1,39 @@
 import React from "react";
+import {useForm} from 'react-hook-form'
 import "./FormContact.css";
 
 const FormContact = () => {
+  const {register, handleSubmit, formState:{errors}} = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      subject: "",
+      content: ""
+    }
+  })
+
   return (
     <div className="center-container">
       <section className="form-design">
-        <form action="">
+        <form onSubmit={handleSubmit((data)=> console.log(data)) }>
           <div className="title-form">
             <h3>Contacto</h3>
           </div>
           <div className="input-group">
             <div className="input-div">
-              <input
-                type="text"
-                placeholder="Escribí acá tu nombre y apellido"
-              />
-              <input
-                type="text"
-                placeholder="Escribí acá tú dirección de mail"
-              />
-              <input
-                type="text"
-                placeholder="Escribí acá el asunto de tu consulta"
-              />
+            <p className="error-message">{errors.name?.type === 'required' && "Por favor, completá tu nombre"}</p>
+            <input {...register("name", {required:true , pattern: /^[A-Za-z]+$/i})} type="text" placeholder="Escribí acá tu nombre apellido" />
+            <p className="error-message">{errors.email?.type === 'required' && "Por favor completá tu email"}</p>
+            <input {...register("email", {required:true, pattern:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/})} type="email" placeholder="Escribí tu dirección email" />
+            <p className="error-message">{errors.subject?.type === 'required' && "Por favor completá el campo asunto"}</p>
+            <input {...register("subject", {required:true})} type="text" placeholder="Escribí el asunto de tu consulta" />
             </div>
             <div className="align-text-form">
               <div className="textA-div">
-                <textarea type="text" placeholder="Escribí acá tú mensaje" />
+            <p className="error-message">{errors.content?.type === 'required' && "Por favor necesitamos esta información para saber sobre tu consulta"}</p>
+            <textarea {...register("content", {required:true})} type="text" placeholder="Escribe un mensaje aquí" />
+               
+                
               </div>
 
             </div>
